6a96db4177911abaf675ab2bfdab6ff1723e0d5d
Beni Nofendi Anwar <beni@Benis-MacBook-Pro-2.local>
2020-11-21 07:13:29 +0700

    initial commit

---
+++ mongo/config/init-mongo.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "${MONGO_USER}",
+        pwd: "${MONGO_PASSWORD}",
+        roles: [
+            {
+                role: "readWrite",
+                db: "${MONGO_INITDB_DATABASE}"
+            }
+        ]
+    }
+);
